20b56f3d90bf2f2ce96b516bafc312833eb1c6a6
Maksymilian Wylaź <maxw.mb78@gmail.com>
2024-09-03 23:55:40 +0200

    Dash Implementation
    Implemented Dashing to mouse position

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -1,16 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rb;
+    Vector2 movement;
+    // Movement Speed Variables
     public float moveSpeed = 200f;
     public float maxSpeed = 500f;
     public float acceleration = 100f;
     public float currentSpeed = 0;
-    Rigidbody2D rb;
-    Vector2 movement;
+
+    // Dash Variables
+    bool canDash = true;
+    bool isDashing = false;
+    public float dashPower = 50f;
+    public float dashTime = 0.2f;
+    public float dashCooldown = 0.5f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,11 +28,26 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if(isDashing)
+        {
+            return;
+        }
+
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     void FixedUpdate()
     {
+        if(isDashing)
+        {
+            return;
+        }
+
         MoveCharacter(movement);
     }
     
@@ -41,5 +66,17 @@ public class PlayerMovement : MonoBehaviour
         currentSpeed = Math.Clamp(currentSpeed, 0, maxSpeed);
         rb.velocity = direction * currentSpeed * Time.deltaTime;*/
         //rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+    }
+
+    IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        Vector2 dashDirection = (Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position)).normalized;
+        rb.velocity = dashDirection * dashPower;
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
